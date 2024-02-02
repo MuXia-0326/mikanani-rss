1afcc0472801ec27416424af382c6075ce975687
MuXia_0326 <504792896@qq.com>
2024-02-02 10:19:47 +0800

    修改代码

--- index.js
+++ index.js
@@ -3,26 +3,21 @@
  * @Date: 2024/02/02
  */
 import express from 'express';
-import querystring from 'querystring';
 import Parser from 'rss-parser';
 
 let parser = new Parser();
 const app = express();
 
 app.get('/RSS/Bangumi', async (req, res) => {
-    console.log(req.query);
-
-    let url = 'https://mikanani.me/RSS/Bangumi?' + querystring.stringify(req.query);
+    let url = 'https://mikanani.me/RSS/Bangumi?' + new URLSearchParams(req.query).toString();
     let feed = await parser.parseURL(url);
 
-    console.log(feed);
-
     feed.items.forEach((item) => {
-        item.link = item.link.replace('https://mikanani.me', 'http://mikanani.mossia.top');
-        item.enclosure.url = item.enclosure.url.replace('https://mikanani.me', 'http://mikanani.mossia.top');
+        item.link = item.link.replace('https://mikanani.me', 'https://mikanani.mossia.top');
+        item.enclosure.url = item.enclosure.url.replace('https://mikanani.me', 'https://mikanani.mossia.top');
     });
 
-    feed.link = feed.link.replace('https://mikanani.me', 'http://mikanani.mossia.top');
+    feed.link = feed.link.replace('http://mikanani.me', 'https://mikanani.mossia.top');
 
     res.json(feed);
 });
